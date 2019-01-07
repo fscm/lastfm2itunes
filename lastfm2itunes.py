3468ae7531fc29802fa671a3ea9302de022f3ae0
Frederico Martins <fredericoscm@gmail.com>
2019-01-07 13:52:48 +0000

    copyright updated

--- lastfm2itunes.py
+++ lastfm2itunes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 #
-# copyright: 2016-2018, Frederico Martins
+# copyright: 2016-2019, Frederico Martins
 # author: Frederico Martins <http://github.com/fscm>
 # license: SPDX-License-Identifier: MIT
 
